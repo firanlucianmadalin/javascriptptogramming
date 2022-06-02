0c50c5f8227a70574e6dde8ad76a4bf481ab230c
firanlucianmadalin <95710495+firanlucianmadalin@users.noreply.github.com>
2022-06-02 20:31:42 +0300

    curs7 curs8 curs9

--- curs7/mycrud/backend/dist/server.js
+++ curs7/mycrud/backend/dist/server.js
@@ -5,11 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+    // res.send('Express + TypeScript Server');
+    res.sendFile(path_1.default.join(__dirname + '/acasa.html'));
 });
 app.listen(port, () => {
     console.log(` [server]: Server is running at http://localhost:${port}`);
--- curs7/mycrud/backend/server.ts
+++ curs7/mycrud/backend/server.ts
@@ -1,5 +1,6 @@
 import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
@@ -7,7 +8,8 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/',(req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+   // res.send('Express + TypeScript Server');
+   res.sendFile(path.join(__dirname+'/acasa.html'));
 });
 
 app.listen(port, () =>{
